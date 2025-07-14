d17dc6e5a6fb768b07a9b3e622e3212f725d410f
KuraharaMasahiro <kuraharamasahiro@gmail.com>
2025-07-14 22:49:47 +0900

    feat: 調整

--- bs-config.js
+++ bs-config.js
@@ -2,7 +2,7 @@ module.exports = {
   files: ['dist/**/*.html', 'dist/**/*.css', 'dist/**/*.js'],
   open: 'external',
   external: true,
-  startPath: 'special/draft2024/',
+  startPath: 'special/draft2025/',
   watch: true,
   server: {
     baseDir: 'dist',
